36be42b85093121cdf9a3fcfa58c2dd541c5c8bc
Pablo Matanza <pablo.matanza@edu.uah.es>
2026-02-26 19:39:26 +0100

    error fixed

--- src/main/java/com/github/pmbdev/global_finance_api/service/UserService.java
+++ src/main/java/com/github/pmbdev/global_finance_api/service/UserService.java
@@ -1,11 +1,12 @@
 package com.github.pmbdev.global_finance_api.service;
 
 import com.github.pmbdev.global_finance_api.controller.dto.AuthResponse;
+import com.github.pmbdev.global_finance_api.controller.dto.RegisterRequest;
 import com.github.pmbdev.global_finance_api.controller.dto.UserResponse;
 import com.github.pmbdev.global_finance_api.repository.entity.UserEntity;
 
 public interface UserService {
-    UserResponse createUser(UserEntity user);
+    UserResponse createUser(RegisterRequest user);
 
     AuthResponse login(String email, String password);
 }
